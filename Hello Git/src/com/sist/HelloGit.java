0dca9059ec91d1fdf39dd887c41bf612c3bf9cde
mambbo <mambbo@naver.com>
2017-02-02 15:19:35 +0900

    added test

--- Hello Git/src/com/sist/HelloGit.java
+++ Hello Git/src/com/sist/HelloGit.java
@@ -4,11 +4,10 @@ public class HelloGit {
 		public static void main(String[] args){
 			System.out.println("Hellp Git");
 			System.out.println("what's Git?");
-
-			System.out.println("It's collaboration");
-			
-
 			System.out.println("It's DVCS");
+			System.out.println("It's collaboration");
+			System.out.println("Test");
+		
 
 		}
 }
